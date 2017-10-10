b1abc759bebb05cc0f2fafba31ad02d5a641a32f
Magi Marinova <magi.marinowax@gmail.com>
2017-10-10 21:37:31 +0300

    msg

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  match '/sums' =>'sums#sums',via: :post
+  match '/sums' =>'tasks#sums',via: :post
+  match '/filters' =>'tasks#filters',via: :post
+  match '/intervals' =>'tasks#intervals',via: :post
+  match '/lin_regressions' =>'tasks#lin_regressions',via: :post
 end
